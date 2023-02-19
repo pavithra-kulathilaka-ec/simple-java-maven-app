bd19554a27322241281384eac26b2f8a5a46361c
Pavithra Kulathilaka <pavithra.kulathilaka@oneflow.com>
2023-02-19 14:08:22 +0530

    Changing the message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello IIdsfT!";
+    private static final String MESSAGE = "Hello IIT!";
 
     public App() {}
 
